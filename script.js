a6e5b097b7cb3c25cac45af2c4700d798e6a5277
Mohit <102155711+ARYAN4ME@users.noreply.github.com>
2023-06-14 23:47:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+function letterCombinations(input) {
+	let tabel = {
+		"2": "abc",
+		"3": "def",
+		"4": "ghi",
+		"5": "jkl",
+		"6": "mno",
+		"7": "pqrs",
+		"8": "tuv",
+		"9": "wxyz"
+	}
+	let solution = [];
+	function solve(currIndex, calStr){
+		if(input.length <= currIndex){
+			solution.push(calStr);
+			return;
+		}
+		let currChar = input[currIndex];
+		for(let i = 0; i<tabel[currChar].length; i++){
+			solve(currIndex++,calStr + tabel[currChar])
+		}
+	}
+	solve(0,"");
+	return solution;
 }
 
 module.exports = letterCombinations;
